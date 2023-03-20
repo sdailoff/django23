d2dbcca397dcd9b117b907a167f3a281dc32db33
Sofia Dailoff <s.sdailoff@gmail.com>
2023-03-20 19:40:37 -0300

    ejs

--- integradores/ej7.py
+++ integradores/ej7.py
@@ -1,15 +1,1 @@
-def maximo_comun_divisor(x, y):
-    temp = 0
-    while y != 0:
-        temp = y
-        y = x % y
-        x = temp
-    return x
-
-
-def minimo_comun_multiplo(x, y):
-    
-    return (x * y) // maximo_comun_divisor(x, y)
-
-print(minimo_comun_multiplo(5, 2))
-    
+#falta hacerlo
